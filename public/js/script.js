6e8653dacdc26f0b70d25dde4e7b9b9db9507b06
Joel O'Connor <joconno07@gmail.com>
2018-10-01 21:51:58 -0600

    Initial main JS file built

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+
+
+const randomColorGenerator = () => ('#' + '0123456789abcdef'.split('').map(function (v, i, a) {
+  return i > 5 ? null : a[Math.floor(Math.random() * 16)]
+}).join(''));
